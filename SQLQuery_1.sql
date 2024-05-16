599e4698d39860a600652ae1bc00d7438f9d337e
Toshi <toshiitiki@gmail.com>
2024-05-15 23:08:16 -0300

    Changes to be committed:
    modified:   SQLQuery_1.sql

--- SQLQuery_1.sql
+++ SQLQuery_1.sql
@@ -1,1 +1,8 @@
-sql
+DECLARE --quando voce declara varias DECLARE no mesmo
+    @idade INT = 39,
+    @Niver DATETIME = '1985-02-06',   
+    @nome VARCHAR(10) = 'Diego';
+SELECT 
+    @nome AS Nome, 
+    @idade AS Idade, 
+    @Niver AS Data_de_Nascimento;

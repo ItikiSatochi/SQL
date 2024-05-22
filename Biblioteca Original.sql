56ce8172eb2ffdd7137ffaa02b6fb53d1acce740
Toshi <toshiitiki@gmail.com>
2024-05-21 23:01:45 -0300

    new file:   Biblioteca Original.sql

---
+++ Biblioteca Original.sql
@@ -0,0 +1,80 @@
+-----PROJETO DE UM BILIOTECA PARA O GITHUB----------
+
+----Criação de banco de dados----
+CREATE DATABASE Biblioteca
+GO
+use Biblioteca;
+GO
+
+----Criação das tabelas----
+CREATE TABLE Autores(
+    Autor_ID            INT PRIMARY KEY IDENTITY,
+    Nome                VARCHAR (100),
+    Sobrenome           VARCHAR(100),
+    Nacionalidade       VARCHAR(100),
+    Data_Nascimento     DATE
+);
+
+CREATE TABLE Editoras (
+    Editora_ID          int PRIMARY KEY IDENTITY,
+    Nome                VARCHAR (100),
+    Pais                VARCHAR (100)
+);
+
+CREATE TABLE Generos (
+    Genero_ID           INT PRIMARY KEY IDENTITY,
+    Descricao           VARCHAR(100)
+)
+
+CREATE TABLE Livros (
+    Livro_ID            INT PRIMARY KEY IDENTITY,
+    Titulo              VARCHAR (100),
+    Publicacao          INT,
+    Autor_ID            INT FOREIGN KEY REFERENCES Autores(Autor_ID),       -- Fazendo uma referencia a tabela Autores  usando o FOREING
+    Editora_ID          INT FOREIGN KEY REFERENCES Editoras(Editora_ID),    -- Fazendo uma referencia a tabela Editores usando o FOREING
+    Genero_ID           INT FOREIGN key REFERENCES Generos(Genero_ID),      -- Fazendo uma referencia a tabela Generos  usando o FOREING
+    ISBN                VARCHAR(20)
+);
+
+CREATE TABLE Usuarios (
+    Usuario_ID          INT PRIMARY KEY IDENTITY,
+    Regiao_ID           INT FOREIGN KEY REFERENCES Regioes(Regiao_ID), -- Fazendo uma referencia a tabela Regiao usando o FOREING
+    Nome                VARCHAR(100),
+    Email               VARCHAR(100),
+    Data_Nascimento     DATE,
+    Endereco            VARCHAR(200)
+);
+
+CREATE TABLE Regioes (   --Nova tabela para adicionar na tabela 
+    Regiao_ID           INT PRIMARY KEY IDENTITY,
+    Cidade              VARCHAR (100),
+    Regiao              VARCHAR (100)
+);
+
+CREATE TABLE Emprestimos (
+    Emprestimo_ID       INT PRIMARY KEY IDENTITY,
+    Usuario_ID          INT FOREIGN key REFERENCES Usuarios(Usuario_ID),    -- Fazendo uma referencia a tabela Usuarios usando o FOREING
+    Livro_ID            INT FOREIGN KEY REFERENCES Livros(Livro_ID),        -- Fazendo uma referencia a tabela Livros usando o  FOREING
+    Data_Emprestimo     DATE,
+    Data_Devolucao      DATE,
+    STATUS              VARCHAR (50)
+);
+
+CREATE TABLE Precos (  ----Nova table para adcionar na tabela 
+    Preco_ID            INT PRIMARY KEY IDENTITY,
+    Livro_ID            INT FOREIGN key REFERENCES Livros(Livro_ID), --Fazendo uma referencia a tabela Livros usando o FOREING
+    Preco_Venda         DECIMAL (10,2), --Preço de venda do livro
+    Preco_Emprestimo    DECIMAL (10,2), --Preço do emprestimo do livro
+    Data_Atualizacao    DATE            --Data da ultima atualizacao do preço
+);
+
+CREATE TABLE Emprestimos_Oline (  ----Nova table para adcionar na tabela 
+    Emprestimo_Online   INT PRIMARY KEY IDENTITY,
+    Livro_ID            INT FOREIGN KEY REFERENCES Livros(Livro_ID),  -- Fazendo uma referencia a tabela Livros usando o FOREING
+    Usuario_ID          INT FOREIGN key REFERENCES Usuarios(Usuario_ID), --Fazendo uma referencia a tabela Usuarios usando o FOREING
+    Data_Emprestimo     DATE,
+    Data_Devolucao      DATE,
+    Valor_Emprestimmo   DECIMAL (10,2), --Valor do emprestimo online
+    STATUS              VARCHAR (50)    -- Status do emprestimos online( Em andamento, Concluido, Atrasado)
+);
+
